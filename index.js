5083abfc306494da396820377556f2e0054fd851
AnshKapoor <ansh605@gmail.com>
2018-07-12 11:45:13 +0530

    Trying to add stylesheet

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var http = require('http');
 var fs = require('fs');
 var express = require('express');
 var app = express();
+app.use(express.static('Assets'));
 app.get('/',function(req,res){
-    res.sendfile(__dirname+'index.html');
+    res.sendFile(__dirname+'/index.html');
 });
 app.listen(3000,function(){
     console.log('running');
